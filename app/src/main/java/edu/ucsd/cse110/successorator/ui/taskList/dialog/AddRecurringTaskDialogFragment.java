37b19209bd04203785b547bf9d0571e2df0d5017
sydneyzhang18 <114766656+sydneyzhang18@users.noreply.github.com>
2024-03-09 23:11:22 -0800

    finalized UI for recurrence dialog

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/taskList/dialog/AddRecurringTaskDialogFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/taskList/dialog/AddRecurringTaskDialogFragment.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
@@ -45,19 +46,60 @@ public class AddRecurringTaskDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         this.view = edu.ucsd.cse110.successorator.databinding.FragmentDialogRecurringMenuBinding.inflate(getLayoutInflater());
         String[] choices = {"one time", "daily", "weekly", "monthly", "yearly"};
-//        final EditText input = (EditText) view.findViewById(R.id.input);
 
-        return new AlertDialog.Builder(getActivity())
-//                .setView(view.getRoot())
+        Button dialogSaveButton = (Button) view.saveButton;
+        Dialog recurDialog = new AlertDialog.Builder(getActivity())
                 .setView(view.getRoot())
-                .setPositiveButton("Save", this::onPositiveButtonClick)
-//                .setPositiveButtonIcon(ContextCompat.getDrawable(getContext(), R.drawable.ic_checkmark))
-
-//                .setSingleChoiceItems(choices, 0, (dialog, which) -> {
-//                })
                 .create();
+        dialogSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("AddRecurringTaskDialogFragment", "This is a debug message");
+                var name = view.recurringInput.getText().toString();
+
+                // Retrieving dialog's selection for recurrence
+                int checkedRadio = view.recurrenceOptions.getCheckedRadioButtonId();
+                String recurrenceText = "";
+                if (checkedRadio != -1) {
+                    View selectedRecurrence = view.recurrenceOptions.findViewById(checkedRadio);
+
+                    int radioIndex = view.recurrenceOptions.indexOfChild(selectedRecurrence);
+                    RadioButton recurrence = (RadioButton) view.recurrenceOptions.getChildAt(radioIndex);
+                    recurrenceText = recurrence.getText().toString();
+                    System.out.println("recurrenceText: " + recurrenceText);
+                }
+
+                // Check that the required input string and radio selection are valid
+                if(!name.equals("") && !recurrenceText.equals(""))
+                {
+                    Calendar cal = CalendarUpdate.getCal();
+                    SimpleDateFormat customFormat = new SimpleDateFormat("EEEE, M/d");
+                    String formattedDate = customFormat.format(cal.getTime());
+                    var dateFormat = DateFormat.getDateInstance(DateFormat.FULL).format(cal.getTime());
+                    MainActivity mainActivity = (MainActivity) getActivity();
+                    var task = new Task(null, name, false, -1, formattedDate, mainActivity.getSpinnerStatus(), TimeUnit.DAYS.toMillis(1),cal.getTimeInMillis());
+                    activityModel.append(task);
+                    recurDialog.dismiss();
+
+                    ((MainActivity)getActivity()).swapFragments();
+
+                    MainViewModel model = ModelFetch.getModel();
+                    if(mainActivity.getSpinnerStatus().equals("Today")){
+                        model.getTodayTasks();
+                    }
+                    else if(mainActivity.getSpinnerStatus().equals("Tomorrow")){
+                        model.getTomorrowTasks();
+                    }
+
+                }
+
+            }
+        });
+
+        return recurDialog;
 
     }
+
 
     private void onPositiveButtonClick(DialogInterface dialog, int which) {
         Log.d("AddRecurringTaskDialogFragment", "This is a debug message");
